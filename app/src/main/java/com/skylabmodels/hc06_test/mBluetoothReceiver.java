2ce63cd77835d7e45f476ac12d71655a0027b157
zufus <marco@irmafamily.net>
2022-08-10 21:06:19 +0200

    Removed unused import declarations

--- app/src/main/java/com/skylabmodels/hc06_test/mBluetoothReceiver.java
+++ app/src/main/java/com/skylabmodels/hc06_test/mBluetoothReceiver.java
@@ -6,9 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 class mBluetoothReceiver extends BroadcastReceiver {
     private static final String TAG = "HC-06 test receiver";
 
